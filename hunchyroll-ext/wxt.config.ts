08d7c4c0e11764c8b17e396b5b01fedd8c0c81df
László GÖRÖG <contact@nergal.xyz>
2025-07-28 14:34:04 +0200

    feat: include gecko id

--- hunchyroll-ext/wxt.config.ts
+++ hunchyroll-ext/wxt.config.ts
@@ -2,25 +2,35 @@ import {defineConfig, defineWebExtConfig} from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte', '@wxt-dev/webextension-polyfill'],
-  manifestVersion: 3,
-  manifest: {
-    name: 'Hunchyroll',
-    default_locale: 'hu',
-    web_accessible_resources: [
-      {
-        resources: ['site.js'],
-        matches: ['https://static.crunchyroll.com/*'],
-      },
-    ],
-    permissions: [
-      "declarativeNetRequest",
-      "storage"
-    ],
-    host_permissions: [
-      "https://static.crunchyroll.com/*",
-      "https://www.crunchyroll.com/watch/*",
-    ],
-  },
+    srcDir: 'src',
+    modules: ['@wxt-dev/module-svelte', '@wxt-dev/webextension-polyfill'],
+    manifestVersion: 3,
+    manifest: ({browser}) => {
+        return {
+            name: 'Hunchyroll',
+            default_locale: 'hu',
+            web_accessible_resources: [
+                {
+                    resources: ['site.js'],
+                    matches: ['https://static.crunchyroll.com/*'],
+                },
+            ],
+            permissions: [
+                "declarativeNetRequest",
+                "storage"
+            ],
+            host_permissions: [
+                "https://static.crunchyroll.com/*",
+                "https://www.crunchyroll.com/watch/*",
+            ],
+            ...(browser === 'firefox' && {
+                browser_specific_settings: {
+                    gecko: {
+                        id: 'contact@nergal.xyz',
+                        strict_min_version: '127.0',
+                    },
+                }
+            })
+        }
+    },
 });
