692ea47014f55802af47a018d67f5ed70016ecea
László GÖRÖG <contact@nergal.xyz>
2025-07-28 13:37:40 +0200

    feat: cache altered crunchyroll bundle

--- hunchyroll-worker/src/endpoints/crunchyrollBundle.ts
+++ hunchyroll-worker/src/endpoints/crunchyrollBundle.ts
@@ -1,35 +1,51 @@
 import {OpenAPIRoute} from "chanfana";
 import {z} from "zod";
 import {type AppContext} from "../types";
-import {HTTPException} from 'hono/http-exception'
 
 export class CrunchyrollBundle extends OpenAPIRoute {
-	schema = {
-		tags: ['Crunchyroll'],
-		summary: 'Show JS bundle',
-		responses: {
-			'200': {
-				description: 'Returns if the task was deleted successfully',
-				content: {
-					'text/javascript': {
-						schema: z.string(),
-					},
-				},
-			},
-		},
-	};
+    schema = {
+        tags: ['Crunchyroll'],
+        summary: 'Show JS bundle',
+        responses: {
+            '200': {
+                description: 'Returns if the task was deleted successfully',
+                content: {
+                    'text/javascript': {
+                        schema: z.string(),
+                    },
+                },
+            },
+        },
+    };
 
-	async handle(c: AppContext) {
-		const resp = await fetch('https://static.crunchyroll.com/vilos-v2/web/vilos/js/bundle.js');
-		if (!resp.ok) {
-			// Invalid response code from Crunchyroll, we might be in big trouble (O.o)
-			return Response.redirect('https://static.crunchyroll.com/vilos-v2/web/vilos/js/bundle.js')
-		}
+    async handle(c: AppContext) {
+        const cache = caches.default
+        let crunchyrollBundle = 'https://static.crunchyroll.com/vilos-v2/web/vilos/js/bundle.js';
+        const cacheKey = crunchyrollBundle
 
-		let text = await resp.text()
-			.then(v => (v.replaceAll(/new Worker\(([^)]*)\)/g, `(() => {const w = new Worker($1); window.hunchyrollWorker = w; return w})()`)))
+        const data = await cache.match(cacheKey)
 
-		c.header('Content-Type', 'text/javascript')
-		return c.body(text)
-	}
+        const resp = await fetch(crunchyrollBundle);
+        if (!resp.ok) {
+            // Invalid response code from Crunchyroll, we might be in big trouble (O.o)
+            return Response.redirect(crunchyrollBundle)
+        }
+
+        if (data && data.headers.get('Last-Modified') === resp.headers.get('Last-Modified')) {
+            return data
+        }
+
+        let text = await resp.text()
+            .then(v => (v.replaceAll(/new Worker\(([^)]*)\)/g, `(() => {const w = new Worker($1); window.hunchyrollWorker = w; return w})()`)))
+
+        let respCache = new Response(text, {
+            headers: {
+                'Content-Type': 'text/javascript',
+                'Last-Modified': resp.headers.get('Last-Modified'),
+                'Cache-Control': resp.headers.get('Cache-Control') || 'max-age=60,must-revalidate',
+            }
+        });
+        await cache.put(cacheKey, respCache.clone())
+        return respCache
+    }
 }
